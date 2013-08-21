565ca25754bdfddd3b8f9ecbfbca26028166f8d6
Kim Silkebækken <kim.silkebaekken@gmail.com>
2013-08-21 10:38:50 +0200

    Add awesome current_tag hierarchy functionality

--- powerline/segments/plugin/tagbar.py
+++ powerline/segments/plugin/tagbar.py
@@ -6,7 +6,16 @@ from powerline.segments.vim import window_cached
 
 
 @window_cached
-def current_tag(pl):
+def current_tag(pl, full_hierarchy=True):
 	if not int(vim.eval('exists(":Tagbar")')):
 		return
-	return vim.eval('tagbar#currenttag("%s", "")')
+	if not full_hierarchy:
+		return vim.eval('tagbar#currenttag("%s", "")')
+	else:
+		tag = [{
+			'contents': tag,
+			'draw_inner_divider': True,
+			'highlight_group': ['current_tag_parent', 'current_tag'],
+			} for tag in vim.eval('tagbar#currenttag("%s", "", "f")').split('.')]
+		tag[-1]['highlight_group'] = ['current_tag']
+		return tag
